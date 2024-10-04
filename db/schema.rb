0b99d1642731b18a036eeb67fa0f7c35ab1347b5
ogustavobrandao <sbrandao.gustavo7@gmail.com>
2024-10-04 00:11:44 -0300

    Definição de seed de user e atualização do schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_01_025436) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_04_021852) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "quartos", force: :cascade do |t|
+    t.integer "numero"
+    t.string "andar"
+    t.float "preco"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reservas", force: :cascade do |t|
+    t.string "cliente_nome"
+    t.string "status"
+    t.date "data_reserva"
+    t.bigint "quarto_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["quarto_id"], name: "index_reservas_on_quarto_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +45,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_01_025436) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reservas", "quartos"
 end
