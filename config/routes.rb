4b6ff63945ba427f7f04cf4b31b3a6647dbc8f06
ogustavobrandao <sbrandao.gustavo7@gmail.com>
2024-10-04 00:08:52 -0300

    Atualização nas rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,18 @@
 Rails.application.routes.draw do
+  resources :reservas
+  resources :quartos
   get 'home/index'
   devise_for :users
 
   devise_scope :user do
-    # Define a rota raiz para quando o usuário não estiver autenticado
     unauthenticated :user do
       root to: 'devise/sessions#new', as: :unauthenticated_root
     end
 
-    # Define a rota raiz para quando o usuário estiver autenticado
     authenticated :user do
       root to: 'home#index', as: :authenticated_root
     end
   end
 
-  # Outras rotas
   get "up" => "rails/health#show", as: :rails_health_check
 end
