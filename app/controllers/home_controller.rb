e7e6b921f84168d067a806b8659f9d14a4d662c8
ogustavobrandao <sbrandao.gustavo7@gmail.com>
2024-10-04 00:07:55 -0300

    Lógica de redirecionamento das homes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    case current_user.role
+    when 'administrador'
+      render 'admins/index'
+    when 'funcionario'
+      render 'funcionarios/index'
+    else
+      root_path
+    end
   end
 end
