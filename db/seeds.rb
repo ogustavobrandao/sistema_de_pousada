734586af216887fe5c62c12e03be1efc8ca875ec
ogustavobrandao <sbrandao.gustavo7@gmail.com>
2024-10-08 02:43:12 -0300

    Atualização do seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,14 @@
 User.create!(
+  nome: 'administrador',
+  cpf: '12345678912',
   email: 'admin@gmail.com',
   password: 'password',
   role: 'administrador'
 )
 
 User.create!(
+  nome: 'funcionario',
+  cpf: '12345678913',
   email: 'funcionario@gmail.com',
   password: 'password',
   role: 'funcionario'
